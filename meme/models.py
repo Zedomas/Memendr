aa3ad31aca3433475beae76e9f1129de92efec79
Zedomas <Zedomas@gmail.com>
2020-08-08 18:45:15 -0500

    Added file structure for memes

--- meme/models.py
+++ meme/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Meme(models.Model):
     title = models.CharField(max_length=75)
-    img = models.URLField(max_length=200)
+    img= models.ImageField(upload_to="meme_pics")
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     favorites = models.IntegerField(default=0)
@@ -14,9 +14,3 @@ class Meme(models.Model):
     def __str__(self):
         return self.title
 
-
-# class Users(models.Model):
-#     username = models.CharField(max_length=50)
-#     email = models.EmailField(max_length=254)
-#     password = models.CharField(max_length=50)
-#     favorites = models.ForeignKey("Meme", on_delete=models.CASCADE)
