31ec468f08994c7d22d8b388d6d4d0b53c772a20
Zedomas <Zedomas@gmail.com>
2020-08-12 12:35:37 -0500

    added more env var for heroku

--- Memendr/settings.py
+++ Memendr/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(s!-b*a=2gcug4jn67gs9mevc$w1*h^6f&_1q6d59$e2xm(!af'
+# SECRET_KEY = '(s!-b*a=2gcug4jn67gs9mevc$w1*h^6f&_1q6d59$e2xm(!af'
+SECRET_KEY = os.environ.get('SECERT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
